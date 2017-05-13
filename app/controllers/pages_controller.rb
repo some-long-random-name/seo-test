e885cecc80e7977c2824b22f903b2d4a1b0e6abb
Eugene Korbut <me@mvl.ru>
2017-05-13 11:33:04 +0700

    Initial page view / controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @pages = Page.sorted
   end
+
+  def show
+    @page = Page.find_by slug: params[:slug]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :pages
   end
+
+  get '/:slug', to: 'pages#show'
 end
